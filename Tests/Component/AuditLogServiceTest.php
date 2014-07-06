7a8d00c417f310923ed0237131048b7593edc412
whitewhidow <iwonderwhy5@hotmail.com>
2014-07-06 03:18:03 +0200

    changes for tests

---
+++ Tests/Component/AuditLogServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BBIT\AuditLogBundle\Tests\Component;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class AuditLogServiceTest extends WebTestCase
+{
+    public function setUp() {
+	$this->client = static::createClient();
+	$this->container = $this->client->getContainer();
+    }
+
+    public function testServiceLoaded()
+    {
+       $this->assertEquals(get_class($this->container->get('bbit_audit_log.service')), 'BBIT\AuditLogBundle\Services\AuditLogService');
+    }
+}
